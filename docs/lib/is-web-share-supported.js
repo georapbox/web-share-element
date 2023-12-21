02366aa28b69b6a6079bcd9ee9b1f60f1999a0d4
georapbox <georapbox@gmail.com>
2023-12-21 11:44:53 +0200

    Update lib files in demo

--- docs/lib/is-web-share-supported.js
+++ docs/lib/is-web-share-supported.js
@@ -1,2 +1,2 @@
-function a(a={}){return a={files:null,...a},Array.isArray(a.files)?"share"in navigator&&"canShare"in navigator&&navigator.canShare(a.files):"share"in navigator}export{a as isWebShareSupported};
+function a(a){return null!==a&&"object"==typeof a?"share"in navigator&&"canShare"in navigator&&navigator.canShare(a):"share"in navigator}export{a as isWebShareSupported};
 //# sourceMappingURL=is-web-share-supported.js.map
